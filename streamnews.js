2a278b4bd1d3a62afba70ec702b09992fd96a19a
yorick <yorick@ctrlaltdelins.xyz>
2017-11-05 23:12:06 +0100

    finished change over to eventregistry

--- streamnews.js
+++ streamnews.js
@@ -5,34 +5,50 @@ var request = require('request');
 var Datastore = require('nedb');
 var db = {};
 db.articles = new Datastore();
+var qs = { apiKey: conf.api_key };
+if (conf.filter) { qs.lang = conf.filters.lang; }
+else { qs.lang = "eng"; }
 
 console.log(clc.bold(' stream')+clc.bgWhite.black.bold('NEWS '));
 
-request({
-  uri: "https://eventregistry.org/json/minuteStreamArticles",
-  rejectUnauthorized: false,
-  useQuerystring: true,
-  qs: {
-      lang: ["eng","fra"],
-      apiKey: conf.api_key
-  }
-}, (error, response, body) => {
-  if (error) { console.log(error); }
-  //if (JSON.parse(body.error)) { console.warn(JSON.parse(body.error)); }
-  let articles = JSON.parse(body).recentActivity.articles.activity;
-  articles.sort((a, b) => {
-    if (a.dateTime < b.dateTime) { return -1; }
-    if (a.dateTime > b.dateTime) { return 1; }
-    return 0;
-  });
-  displayArticles(articles);
-});
-
-function displayArticles (articles) {
-  articles.map((article) => {
-    if (article) {
-      //console.log(clc.green(article.time) + ' | ' + clc.blue(article.source.title) + ' | ' + article.title);
+function getArticles () {
+  request({
+    uri: "https://eventregistry.org/json/minuteStreamArticles",
+    rejectUnauthorized: false,
+    useQuerystring: true,
+    qs: qs
+  }, (error, response, body) => {
+    if (error) { console.log(error); }
+    body = JSON.parse(body);
+    if (!body.error) {
+      let articles = body.recentActivity.articles.activity;
+      articles.sort((a, b) => {
+        if (a.dateTime < b.dateTime) { return -1; }
+        if (a.dateTime > b.dateTime) { return 1; }
+        return 0;
+      });
+      displayArticles(articles);
     }
-    console.log(article);
+    else {
+      console.warn(body.error);
+    }
   });
 }
+
+function displayArticles (articles) {
+  let collection = [];
+  articles.map((article) => {
+    if (article) {
+      let promise = article;
+      console.log(clc.green(article.time) + ' | ' + clc.blue(article.source.title) + ' | ' + article.title);
+      qs.id = article.id;
+      collection.push(article);
+    }
+  });
+
+  Promise.all(collection).then(() => {
+    setTimeout(() => { getArticles(); },50000);
+  }).catch((error) => { reject(error); });
+}
+
+getArticles();
